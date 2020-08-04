45b0cf669edb3dee072396fbddcf0ba479588236
liuyuefeng <liuyf935@chinaunicom.cn>
2020-08-04 11:12:43 +0800

    oauthService:持久化token等信息

--- server-auth/src/main/java/org/liuyuefeng/security/serverauth/OAuth2AuthServer.java
+++ server-auth/src/main/java/org/liuyuefeng/security/serverauth/OAuth2AuthServer.java
@@ -1,14 +1,20 @@
 package org.liuyuefeng.security.serverauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
@@ -17,10 +23,17 @@ public class OAuth2AuthServer extends AuthorizationServerConfigurerAdapter {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private AuthenticationManager authenticationManager;
-
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public TokenStore tokenStore(){
+        return new JdbcTokenStore(dataSource);
+    }
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager);
     }
 
 
@@ -38,10 +51,13 @@ curl --location --request POST 'http://localhost:9090/oauth/token' \
 
 sourceServer验证token接口是 /oauth/check_token
  */
+public static void main(String[] args) {
+    System.out.println(new BCryptPasswordEncoder().encode("123456"));
+}
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
+ /*       clients.inMemory()
                 .withClient("orderApp")
                 .secret(passwordEncoder.encode("123456"))
                 .scopes("read", "write")
@@ -55,7 +71,9 @@ sourceServer验证token接口是 /oauth/check_token
                 .accessTokenValiditySeconds(3600)
                 .resourceIds("order-service")
                 .authorizedGrantTypes("password");
-    }
+*/
+ clients.jdbc(dataSource);
+}
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
