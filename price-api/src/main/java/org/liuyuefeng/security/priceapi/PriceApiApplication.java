cdb09df26529cf93d68bacaad51af5acc244b55a
liuyuefeng <liuyf935@chinaunicom.cn>
2020-08-10 07:30:48 +0800

    clear for sentinel

--- price-api/src/main/java/org/liuyuefeng/security/priceapi/PriceApiApplication.java
+++ price-api/src/main/java/org/liuyuefeng/security/priceapi/PriceApiApplication.java
@@ -2,7 +2,8 @@ package org.liuyuefeng.security.priceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//
+// import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 //@EnableResourceServer
