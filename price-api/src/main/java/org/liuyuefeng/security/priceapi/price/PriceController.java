32ab690423ca166a31733ca97cef5829711fb8b0
liuyuefeng <liuyf935@chinaunicom.cn>
2020-08-10 22:50:18 +0800

    sentinel：Springboot方式，接入sentinel-dashboard

--- price-api/src/main/java/org/liuyuefeng/security/priceapi/price/PriceController.java
+++ price-api/src/main/java/org/liuyuefeng/security/priceapi/price/PriceController.java
@@ -3,6 +3,7 @@ package org.liuyuefeng.security.priceapi.price;
 
 import com.alibaba.csp.sentinel.Entry;
 import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 //import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,21 +15,15 @@ import java.math.BigDecimal;
 @Slf4j
 public class PriceController {
     @GetMapping("/{id}")
+    @SentinelResource("price")
     public PriceInfo create(@PathVariable Long id
             //, @AuthenticationPrincipal String username
     ){
-
-
-        try(Entry entry = SphU.entry("price")){
             // log.info("user is " + username);
             log.info("productId is " + id);
             PriceInfo info = new PriceInfo();
             info.setId(id);
             info.setPrice(new BigDecimal(100));
             return info;
-        } catch (BlockException e) {
-            log.info("Blocked");
-            return null;
-        }
     }
 }
